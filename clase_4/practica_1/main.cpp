2d27a4ce4f7f42038e4b84969b6be910d5d0267d
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2023-09-26 09:22:09 -0600

    Clase 5 finalizada

--- clase_4/practica_1/main.cpp
+++ clase_4/practica_1/main.cpp
@@ -8,10 +8,13 @@ int main()
     cout << "Ingresa el numero límite de la Serie de Fibonacci" << endl;
     cin >> limit;
     int bucket;
-
+    int suma = 1;
+    int sumaa = 0;
     for (int i = 0; i <= limit; i++)
     {
-        bucket = bucket + i;
+        bucket = suma + sumaa;
+        sumaa = suma;
+        suma = bucket;
         cout << "El acumulado es de: " << bucket << endl;
     }
     return 0;
--- clase_4/practica_2/main.cpp
+++ clase_4/practica_2/main.cpp
@@ -6,10 +6,15 @@ int main()
     cout << "Ingresa el numero límite de la Serie de Fibonacci" << endl;
     cin >> limit;
     int bucket;
+    int suma=1;
+    int sumaa=0;
     int i = 0;
     while (i <= limit)
     {
-        bucket = bucket + i;
+
+        bucket = suma + sumaa;
+        sumaa = suma;
+        suma = bucket;
         cout << "El acumulado es de: " << bucket << endl;
         i++;
     }
