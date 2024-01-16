6c9f51f0c4a5e2af6b3f349039849ee1222c1bbd
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2024-01-16 12:20:39 -0600

    Practica 1

---
+++ clase_2/practica_1/main.java
@@ -0,0 +1,23 @@
+// Programa que usa operadores lógicos
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Noel Rincón Anaya\n181233\n");
+
+        float num1;
+        float num2;
+        float R;
+        num1 = 25;
+        num2 = 33;
+        R = num1 + num2;
+        
+        System.out.println("La suma es: " + R);
+        R = num1 - num2;
+        System.out.println("La resta es: " + R);
+        R = num1 * num2;
+        System.out.println("La multiplicacion es: " + R);
+        R = num1 / num2;
+        System.out.println("La division es: " + R);
+
+    }
+}
