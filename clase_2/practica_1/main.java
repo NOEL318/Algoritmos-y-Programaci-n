386d9c35e78d296d8004e132fa22d689b23f9532
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2024-01-16 12:35:20 -0600

    Calculadora de 2 datos

--- clase_2/practica_1/main.java
+++ clase_2/practica_1/main.java
@@ -1,16 +1,22 @@
+
 // Programa que usa operadores lógicos
+import java.util.Scanner;
+import java.math.*;
 
 public class main {
     public static void main(String[] args) {
         System.out.println("Noel Rincón Anaya\n181233\n");
 
-        float num1;
-        float num2;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el valor numero 1: ");
+        float num1 = sc.nextFloat();
+        System.out.println("Ingrese el valor numero 2: ");
+        float num2 = sc.nextFloat();
+        double potenciaraiz;
         float R;
-        num1 = 25;
-        num2 = 33;
+
         R = num1 + num2;
-        
         System.out.println("La suma es: " + R);
         R = num1 - num2;
         System.out.println("La resta es: " + R);
@@ -19,5 +25,12 @@ public class main {
         R = num1 / num2;
         System.out.println("La division es: " + R);
 
+        potenciaraiz = Math.pow(num1, num2);
+        System.out.println("La potencia es: " + potenciaraiz);
+
+        potenciaraiz = Math.sqrt(num1);
+        System.out.println("La raiz es: " + potenciaraiz);
+
+        sc.close();
     }
 }
