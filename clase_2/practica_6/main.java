b28c222308ae9b3b672c90c8839df49efe467bdc
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2024-01-16 13:32:38 -0600

    Clase 2 finalizada

---
+++ clase_2/practica_6/main.java
@@ -0,0 +1,28 @@
+
+// Programa que califica promedios
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Noel Rincón Anaya\n181233\n");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese la calificacion numero 1: ");
+        float cal1 = sc.nextFloat();
+        System.out.println("Ingrese la calificacion numero 2: ");
+        float cal2 = sc.nextFloat();
+        System.out.println("Ingrese la calificacion numero 3: ");
+        float cal3 = sc.nextFloat();
+
+        float promedio = (cal1 + cal2 + cal3) / 3;
+
+        if (promedio > 7.5) {
+            System.out.println("Usted aprobó el curso con promedio de: "+promedio);
+        }
+        else{
+            System.out.println("Usted NO aprobó el curso con promedio de: "+promedio);
+        }
+
+        sc.close();
+    }
+}
